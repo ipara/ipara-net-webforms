2621b64071f52391cd8d0f2a041e8693f60cd533
ininal <burakzago96@hotmail.com>
2023-09-13 19:22:59 +0300

    Implement bin inquiry v4 and add checkout form creation request and response

---
+++ IparaPaymentDemo/BinInquiryV4.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BinInquiryV4.aspx.cs" Inherits="IparaPaymentDemo.BinInquiryV4" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="SiteContent" runat="server">
+    
+        <fieldset>
+
+		<!-- Form Name -->
+		<legend>Bin Sorgulama V4</legend>
+
+		<!-- Text input-->
+		<div class="form-group">
+			<label class="col-md-4 control-label" for="binNumber">Bin Numarası (*):</label>
+			<div class="col-md-4">
+				<input id="binNumber" type="text" placeholder=""  runat="server" class="form-control input-md" required="">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label class="col-md-4 control-label" for="binNumber">Tutar (**):</label>
+			<div class="col-md-4">
+				<input id="amount" type="text" placeholder="" runat="server" class="form-control input-md" required="">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label class="col-md-4 control-label" for="binNumber">3D / (Non-Secure) (*):</label>
+			<div class="col-md-4">
+				<select id="threeD" type="text" placeholder="" runat="server" class="form-control" >
+					<option value="true">3D</option>
+					<option value="false">Non-Secure</option>
+				</select>
+			</div>
+		</div>
+
+		<!-- Button -->
+		<div class="form-group">
+			<label class="col-md-4 control-label" for=""></label>
+			<div class="col-md-4">
+				<asp:Button ID="BtnInquiry" runat="server" Text="Sorgula" class="btn btn-success" OnClick="BtnInquiry_Click" />
+			</div>
+		</div>
+		<p>** Sipariş tutarı kuruş ayracı olmadan gönderilmelidir. Örneğin; 1 TL 100, 12 1200, 130 13000, 1.05 105, 1.2 120 olarak gönderilmelidir.</p>
+	</fieldset>
+
+    <div id="result" runat="server"></div>
+</asp:Content>
---
+++ IparaPaymentDemo/CheckoutFormCreate.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using IPara.DeveloperPortal.Core.Request;
+using IPara.DeveloperPortal.Core.Response;
+using IparaPayment;
+using IparaPayment.Entity;
+using Newtonsoft.Json;
+
+namespace IparaPaymentDemo
+{
+    public partial class CheckoutFormCreate : System.Web.UI.Page
+    {    
+
+        protected void BtnCreateCheckoutForm_Click(object sender, EventArgs e)
+        {
+            Settings settings = new();
+            CheckoutFormCreateRequest request = new();
+            request.OrderId = Guid.NewGuid().ToString();
+            request.Version = settings.Version;
+            request.Amount = 10000;
+            request.CallbackUrl = "https://apitest.ipara.com/rest/payment/threed/test/result";
+            request.VendorId = "10100";
+            request.Threed = "false";
+            request.AllowedInstallments = new HashSet<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+
+            request.Echo = "";
+            request.Mode = settings.Mode;
+            request.Language = "tr-TR";
+
+            request.Purchaser = new();
+            request.Purchaser.Name = "Murat";
+            request.Purchaser.SurName = "Kaya";
+            request.Purchaser.BirthDate = "1986-07-11";
+            request.Purchaser.Email = "murat@kaya.com";
+            request.Purchaser.GsmPhone = "5881231212";
+            request.Purchaser.IdentityNumber = "12345678901";
+            request.Purchaser.ClientIp = "127.0.0.1";
+
+            request.Purchaser.InvoiceAddress = new();
+            request.Purchaser.InvoiceAddress.Name = "Murat";
+            request.Purchaser.InvoiceAddress.SurName = "Kaya";
+            request.Purchaser.InvoiceAddress.Address = "Mevlüt Pehlivan Mah. Multinet Plaza Şişli";
+            request.Purchaser.InvoiceAddress.ZipCode = "34782";
+            request.Purchaser.InvoiceAddress.CityCode = "34";
+            request.Purchaser.InvoiceAddress.IdentityNumber = "12345678901";
+            request.Purchaser.InvoiceAddress.CountryCode = "TR";
+            request.Purchaser.InvoiceAddress.TaxNumber = "123456";
+            request.Purchaser.InvoiceAddress.TaxOffice = "Kozyatağı";
+            request.Purchaser.InvoiceAddress.CompanyName = "iPara";
+            request.Purchaser.InvoiceAddress.PhoneNumber = "2122222222";
+
+            request.Purchaser.ShippingAddress = new();
+            request.Purchaser.ShippingAddress.Name = "Murat";
+            request.Purchaser.ShippingAddress.SurName = "Kaya";
+            request.Purchaser.ShippingAddress.Address = "Mevlüt Pehlivan Mah. Multinet Plaza Şişli";
+            request.Purchaser.ShippingAddress.ZipCode = "34782";
+            request.Purchaser.ShippingAddress.CityCode = "34";
+            request.Purchaser.ShippingAddress.IdentityNumber = "12345678901";
+            request.Purchaser.ShippingAddress.CountryCode = "TR";
+            request.Purchaser.ShippingAddress.PhoneNumber = "2122222222";
+
+            request.Products = new List<Product>();
+            Product p = new();
+            p.Title = "Telefon";
+            p.Code = "TLF0001";
+            p.Price = "5000";
+            p.Quantity = 1;
+            request.Products.Add(p);
+
+            p = new();
+            p.Title = "Bilgisayar";
+            p.Code = "BLG0001";
+            p.Price = "5000";
+            p.Quantity = 1;
+            request.Products.Add(p);
+
+            CheckoutFormCreateResponse response = CheckoutFormCreateRequest.Execute(request, settings);
+            string jsonResponse = JsonConvert.SerializeObject(response, Formatting.Indented);
+            string encodedJsonResponse = System.Web.HttpUtility.HtmlEncode(jsonResponse);
+            result.InnerHtml = "<pre>" + encodedJsonResponse + "</pre>";
+        }
+    }
+}
